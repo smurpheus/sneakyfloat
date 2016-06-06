34c353c398ffb14177d527b5f3a30ca254cd3a48
Maximilian H <mhertzer@tzi.de>
2016-06-07 01:25:22 +0200

    db fitted for new deal design

--- FloatFetcher.py
+++ FloatFetcher.py
@@ -85,9 +85,14 @@ class FloatFetcher(EventEmitter):
             with self.db_lock:
                 lines = file.readlines()
                 for line in lines[1:]:
-                    item, number, maxfloat, maxprice = line.split(";")
-                    item = item.replace(" ","")
+                    item, number, maxfloat, maxprice, buygrp, dealno, dealname = line.split(";")
+                    item = item.replace(" ", "")
                     print("%s %s,%s,%s,%s" % (lines.index(line), item.replace(" ",""), number, maxfloat, maxprice))
+                    deal = db.get_deal_by_name(dealname)
+                    if not deal:
+                        db.create_deal(dealname)
+                        deal = db.get_deal_by_name(dealname)
+                    
                     indb = db.get_buy_order(item, maxfloat)
                     if not indb:
                         db.create_buy_order(item, number,maxfloat,maxprice)
--- dbconnector.py
+++ dbconnector.py
@@ -17,6 +17,65 @@ class DBConnector(object):
         cur.execute(statement)
         self.con.commit()
 
+    def get_deal_by_name(self, name):
+        cur = self.con.cursor()
+        results = cur.execute("SELECT * from deals WHERE name='%s'" % name).fetchall()
+        if len(results) < 1:
+            return False
+        result = results[0]
+        return result
+
+    def get_deal_by_id(self, id):
+        cur = self.con.cursor()
+        results = cur.execute("SELECT * from deals WHERE id='%s'" % id).fetchall()
+        if len(results) < 1:
+            return False
+        result = results[0]
+        return result
+
+    def get_all_deals(self):
+        cur = self.con.cursor()
+        results = cur.execute("SELECT * from deals").fetchall()
+        if len(results) < 1:
+            return False
+        result = results
+        return result
+
+    def get_all_grps(self):
+        cur = self.con.cursor()
+        results = cur.execute("SELECT * from buygroup").fetchall()
+        if len(results) < 1:
+            return False
+        result = results
+        return result
+
+    def get_deal_grp_by(self, key, value):
+        cur = self.con.cursor()
+        results = cur.execute("SELECT * from buygroup WHERE %s='%s'" %(key, value)).fetchall()
+        if len(results) < 1:
+            return False
+        result = results[0]
+        return result
+
+    def create_deal_grp(self, grp_id, number, floatv, deal_id, price):
+        statement = "INSERT INTO buygroup VALUES(%s,%s,%s,%s,%s)" % (grp_id, number, floatv, deal_id, price)
+        cur = self.con.cursor()
+        cur.execute(statement)
+        self.con.commit()
+
+    def create_deal(self, name):
+        alldeals = self.get_all_deals()
+        max_id = max(x[0] for x in alldeals)
+        max_id += 1
+        names = [x for x in alldeals if x[1] == name]
+        if names < 1:
+            statement = "INSERT INTO deals VALUES(%s,'%s')" % (name, max_id)
+            cur = self.con.cursor()
+            cur.execute(statement)
+            self.con.commit()
+            return True
+        return False
+
     def get_listing_by_id(self, listing_id):
         cur = self.con.cursor()
         results = cur.execute("SELECT * from listings WHERE Id=%s" % listing_id).fetchall()
@@ -41,19 +100,19 @@ class DBConnector(object):
                     url=result[9], fee=result[10]))
         return listings
 
-    def create_buy_order(self, item, number, maxfloat, maxprice):
-        result = self.get_buy_order(item, maxfloat)
+    def create_buy_order(self, item, grp_id):
+        result = self.get_buy_order(item, grp_id)
         if result:
             return False
-        statement = "INSERT INTO buyorders VALUES ('%s', %s, %s,%s)" % (item, number, maxfloat, maxprice)
+        statement = "INSERT INTO buyorders VALUES ('%s', %s)" % (item, grp_id)
         print(statement)
         cur = self.con.cursor()
         cur.execute(statement)
         self.con.commit()
 
-    def get_buy_order(self, item, maxfloat):
+    def get_buy_order_by(self, key, value):
         cur = self.con.cursor()
-        statement = "SELECT * FROM buyorders where item='%s' and maxfloat=%s" % (item, maxfloat)
+        statement = "SELECT * FROM buyorders where %s='%s'" % (key, value)
         results = cur.execute(statement).fetchall()
         if len(results) < 1:
             return False
